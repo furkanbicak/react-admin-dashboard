e46cba7cb13aaf1234b8cf204d5c29f91eb02439
afurkanbicak <afurkanbicak@gmail.com>
2023-07-21 00:56:57 +0300

    pie chart box added

---
+++ src/components/pieChartBox/PieChartBox.tsx
@@ -0,0 +1,47 @@
+import { Cell, Pie, PieChart, ResponsiveContainer, Tooltip } from 'recharts'
+import "./pieChartBox.scss";
+
+const data = [
+    { name: "Mobile", value: 400, color:'#0088FE' },
+    { name: "Desktop", value: 400, color:'#00C49F' },
+    { name: "Laptop", value: 400, color:'#FFBB28' },
+    { name: "Tablet", value: 400, color:'#FF8042' },
+]
+export const PieChartBox = () => {
+    return (
+        <div className="pieChartBox">
+          <h1>Leads by Source</h1>
+          <div className="chart">
+            <ResponsiveContainer width="99%" height={300}>
+              <PieChart>
+                <Tooltip
+                  contentStyle={{ background: "white", borderRadius: "5px" }}
+                />
+                <Pie
+                  data={data}
+                  innerRadius={"70%"}
+                  outerRadius={"90%"}
+                  paddingAngle={5}
+                  dataKey="value"
+                >
+                  {data.map((item) => (
+                    <Cell key={item.name} fill={item.color} />
+                  ))}
+                </Pie>
+              </PieChart>
+            </ResponsiveContainer>
+          </div>
+          <div className="options">
+            {data.map((item) => (
+              <div className="option" key={item.name}>
+                <div className="title">
+                  <div className="dot" style={{ backgroundColor: item.color }} />
+                  <span>{item.name}</span>
+                </div>
+                <span>{item.value}</span>
+              </div>
+            ))}
+          </div>
+        </div>
+      );
+}
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,5 +1,6 @@
 import BarChartBox from '../../components/barChartBox/BarChartBox';
 import { ChartBox } from '../../components/chartBox/ChartBox';
+import { PieChartBox } from '../../components/pieChartBox/PieChartBox';
 import { TopBox } from '../../components/topBox/TopBox';
 import { 
     barChartBoxRevenue, 
@@ -18,7 +19,7 @@ export const Home = () => {
             </div>
             <div className='box box2'><ChartBox {...chartBoxUser} /></div>
             <div className='box box3'><ChartBox {...chartBoxProduct}/></div>
-            <div className='box box4'>Box 3</div>
+            <div className='box box4'><PieChartBox /></div>
             <div className='box box5'><ChartBox {...chartBoxConversion}/></div>
             <div className='box box6'><ChartBox {...chartBoxRevenue}/></div>
             <div className='box box7'>Box 7</div>
